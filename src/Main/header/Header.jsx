69f56b6b501ba02820013c33b17ea1452973137d
Ezz-Coding <ezzrushdi1@gmail.com>
2024-11-21 21:51:57 +0300

    update

--- src/Main/header/Header.jsx
+++ src/Main/header/Header.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import './header.css'
 import imagemenu from '../../assets/menu-line (1).svg';
-import imagemore from '../../assets/more-2-line.svg';
+
 import './../navber/navber.css'
-import Navber from '../navber/Navber';
+
 const Header = ({click,setClick}) => {
 
 
@@ -13,7 +13,7 @@ const Header = ({click,setClick}) => {
     const  Menu =document.getElementById("imageMenu");
     const  h1 =document.getElementById("h1");
     
-console.log(navbar);
+
     navbar.style.right=click? '-1%':
    '-100%';
    Menu.style.display=click?'none':
